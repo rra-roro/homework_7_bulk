8152262beeec112c144594c1867e8363039b628d
rra.roro@gmail.com <rra.roro@gmail.com>
2019-04-14 02:56:29 +0300

    have added publisher_mixin class

---
+++ include/publisher.h
@@ -0,0 +1,95 @@
+﻿#pragma once
+#include <set>
+#include <functional>
+#include <type_traits>
+
+namespace roro_lib
+{
+      namespace internal
+      {
+            inline static auto fn_comp = [](auto& fn1, auto& fn2) {
+                  return &fn1 < &fn2;
+            };
+      }
+
+      template <typename>
+      class publisher_mixin;
+
+      template <typename R, typename... Args>
+      class publisher_mixin<R(Args...)>
+      {
+            std::set<std::function<R(Args...)>, decltype(internal::fn_comp)> subscribers{ internal::fn_comp };
+
+            template <std::size_t I = 0,
+                typename T,
+                typename F = R (T::*)(Args...),
+                std::size_t... PhNumber>
+            constexpr void add_subscriber_internal(const T& obj, F fn)
+            {
+                  if constexpr (I < sizeof...(Args))
+                  {
+                        add_subscriber_internal<I + 1, T, F, I, PhNumber...>(obj, fn);
+                  }
+                  else
+                  {
+                        using namespace std::placeholders;
+                        constexpr auto placeholders_tuple = std::make_tuple(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10);
+                        subscribers.insert(std::bind(fn, obj, std::get<PhNumber>(placeholders_tuple)...));
+                  }
+            }
+
+            template <typename, typename Facke = void>
+            struct test_arg_subscriber : std::false_type
+            {
+            };
+
+            template <typename Rеt, typename... A>
+            struct test_arg_subscriber<Rеt (*)(A...), std::enable_if_t<std::is_same_v<std::tuple<A...>, std::tuple<Args...>>>> : std::true_type
+            {
+            };
+
+            template <typename Rеt, typename C, typename... A>
+            struct test_arg_subscriber<Rеt (C::*)(A...), std::enable_if_t<std::is_same_v<std::tuple<A...>, std::tuple<Args...>>>> : std::true_type
+            {
+            };
+
+            template <class _Ty>
+            static constexpr bool test_arg_subscriber_v = test_arg_subscriber<_Ty>::value;
+
+        public:
+            template <typename F,
+                typename std::enable_if_t<std::is_pointer_v<F> &&
+                                          std::is_function_v<typename std::remove_pointer_t<F>>>* Facke = nullptr>
+            void add_subscriber(F fn)
+            {
+                  static_assert(test_arg_subscriber_v<F>);
+                  subscribers.insert(fn);
+            }
+
+            template <typename T, typename MF,
+                typename std::enable_if_t<std::is_member_function_pointer_v<MF>>* Facke = nullptr>
+            void add_subscriber(const T& obj, MF mfn)
+            {
+                  static_assert(test_arg_subscriber_v<MF>);
+
+                  add_subscriber_internal(obj, mfn);
+            }
+
+            template <typename T,
+                typename std::enable_if_t<std::is_member_function_pointer_v<decltype(&T::operator())>>* Facke = nullptr>
+            void add_subscriber(const T& obj)
+            {
+                  static_assert(test_arg_subscriber_v<decltype(&T::operator())>);
+                  add_subscriber(obj, &T::operator());
+            }
+
+
+            void notify(Args... args)
+            {
+                  for (auto& subscriber : subscribers)
+                  {
+                        subscriber(args...);
+                  }
+            }
+      };
+}
---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+﻿#include "publisher.h"
+#include <iostream>
+#include <exception>
+
+#include "lib_version.h"
+#include "CLParser.h"
+
+
+using namespace std;
+using namespace roro_lib;
+
+void help()
+{
+      cout << R"(
+ This demo is my bulk.
+
+    bulk  [-version | -? | -math_oder_dimensions]
+       Options:  
+       -version                -get version of program
+       -?                      -about program (this info)
+       -math_oder_dimensions   -output dimensions at math order (like x, y). Example: column, row   
+                                By Default: row, column
+)" << endl;
+}
+
+void version_bulk()
+{
+      cout << "Version bulk: " << version() << endl;
+}
+
+class publisher : public publisher_mixin<void(int)>
+{
+};
+
+struct subscriber1
+{
+      void operator()(int i)
+      {
+            std::cout << "subscriber1_op=" << i << "\n";
+      }
+
+      void fn(int i)
+      {
+            std::cout << "subscriber1_fn=" << i << "\n";
+      }
+};
+
+void subscriber2(int i)
+{
+      std::cout << "subscriber2=" << i << "\n";
+}
+
+#ifndef _TEST
+
+int main(int argc, char* argv[])
+{
+      try
+      {
+            ParserCommandLine PCL;
+            PCL.AddFormatOfArg("?", no_argument, '?');
+            PCL.AddFormatOfArg("help", no_argument, '?');
+            PCL.AddFormatOfArg("version", no_argument, 'v');
+            PCL.AddFormatOfArg("math_oder_dimensions", no_argument, 'm');
+
+            PCL.SetShowError(false);
+            PCL.Parser(argc, argv);
+
+            if (PCL.Option['?'])
+            {
+                  help();
+                  return 0;
+            }
+            if (PCL.Option['v'])
+            {
+                  version_bulk();
+                  return 0;
+            }
+
+            publisher pub;
+
+            subscriber1 sub;
+
+            pub.add_subscriber(sub, &subscriber1::fn);
+            pub.add_subscriber(subscriber2);
+            pub.add_subscriber(sub);
+
+            pub.notify(5);
+
+      }
+      catch (const exception& ex)
+      {
+            cerr << "Error: " << ex.what() << endl;
+            return EXIT_FAILURE;
+      }
+      catch (...)
+      {
+            cerr << "Error: unknown exception" << endl;
+            return EXIT_FAILURE;
+      }
+
+      return 0;
+}
+
+#endif
